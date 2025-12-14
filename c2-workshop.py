899ce243ed46af897bf627be3a3e800a58c61b30
ishankfl <ishankafle5@gmail.com>
2025-12-14 14:56:27 +0545

    mqtt-connected-successfully

--- c2-workshop.py
+++ c2-workshop.py
@@ -1,5 +1,8 @@
 import network
 import time
+from umqtt.simple import MQTTClient
+import ujson
+
 # connection to wifi
 wifi_connection = network.WLAN(network.STA_IF)
 wifi_connection.active(True)
@@ -11,3 +14,18 @@ while not wifi_connection.isconnected():
     print('.')
     time.sleep(.5)
 print("Connected successfully")
+
+MQTT_CLIENT_ID = 'yourname-clientid-123'
+MQTT_BROKER = 'broker.mqttdashboard.com'
+MQTT_TOPIC = 'yourname-topic-something'
+#connection to mqtt
+mqtt_publisher = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER)
+mqtt_publisher.connect()
+
+i = 0
+while True:
+    data = {'number':i}
+    payload = ujson.dumps(data) # dictionary converted to json
+    mqtt_publisher.publish(MQTT_TOPIC, payload)
+    i+=1
+    print("sent data", payload)
