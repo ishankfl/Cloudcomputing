7066f59bffe45dc1a9fd8145e7e6bcaf583401d7
ishankfl <ishankafle5@gmail.com>
2025-12-14 08:55:07 +0545

    data-received-on-wokwi

--- node-red-to-wokwi.py
+++ node-red-to-wokwi.py
@@ -1,4 +1,6 @@
 import network 
+import time
+from umqtt.simple import MQTTClient 
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -6,4 +8,25 @@ wifi.connect('Wokwi-GUEST','')
 print("Connecting.")
 while not wifi.isconnected():
     print('.',end=' ')
+    time.sleep(.4)
 print("Connected successfully")
+
+MQTT_CLIENT_ID = 'yourname-clientid-randomnumber'
+MQTT_SERVER = 'broker.mqttdashboard.com'
+MQTT_TOPIC = 'ishan-heloo123-led'
+
+def msg_received(topic, msg):   #callback function
+    print("MSG Received", msg)
+
+mqtt_subscriber = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER)
+mqtt_subscriber.set_callback(msg_received)
+mqtt_subscriber.connect()
+
+mqtt_subscriber.subscribe(MQTT_TOPIC)
+
+print("Mqtt connected successfully")
+
+while True:
+    mqtt_subscriber.check_msg()
+    time.sleep(.5)
+    print('.')
