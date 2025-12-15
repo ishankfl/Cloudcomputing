09f7fdeea039f6e632161d45fe08007d6bc0fde5
ishankfl <ishankafle5@gmail.com>
2025-12-15 09:08:28 +0545

    on-off-button-handle-esp

--- c9-workhsop.py
+++ c9-workhsop.py
@@ -1,6 +1,10 @@
 import network
 from umqtt.simple import MQTTClient
 import time 
+from machine import Pin
+
+#define LED
+led_pin = Pin(5, Pin.OUT)
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -11,12 +15,17 @@ while not wifi.isconnected():
     print("Connecting..")
 print("Connected to wifi")
 
-MQTT_CLIENT_ID = ''
+MQTT_CLIENT_ID = 'mqtt-client-sulav-123'
 MQTT_BROKER = 'broker.mqttdashboard.com'
-MQTT_TOPIC = ''
+MQTT_TOPIC = 'mqtt-topic-randomnamevalue'
 
 def received_msg(topic, msg):
     print("Msg", msg)
+    decoded_msg = msg.decode()
+    if decoded_msg == 'ON':
+        led_pin.value(1)
+    if decoded_msg == 'OFF':
+        led_pin.value(0)
 
 mqtt_subscriber = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER)# client_id, server-url
 mqtt_subscriber.set_callback(received_msg)
