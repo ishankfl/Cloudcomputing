01bfe35685f8a0a6cd862c40deb05a09487b0f60
ishankfl <ishankafle5@gmail.com>
2025-12-15 15:37:16 +0545

    led-control-done-by-esp

--- c14-workshop.py
+++ c14-workshop.py
@@ -1,6 +1,9 @@
 import network
 import time 
 from umqtt.simple import MQTTClient 
+from machine import Pin
+#setup pin
+led_light = Pin(17, Pin.OUT)
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -13,12 +16,20 @@ while True:
     time.sleep(.3)
 print("Wifi connected successfully")
 
-mqtt_client_id = ''
+mqtt_client_id = 'ishan-client-12'
 mqtt_broker = 'broker.mqttdashboard.com'
-mqtt_topic = ''
+mqtt_topic = 'ishan-topic-12'
 
 def msg_receiver(topic, message):
     print("Incomming Msg", message)
+    message = message.decode()
+
+    if message == 'ON':
+        led_light.value(1)
+    
+    elif message == 'OFF':
+        led_light.value(0)
+
 
 mqtt_subscriber = MQTTClient(mqtt_client_id, mqtt_broker)
 mqtt_subscriber.set_callback(msg_receiver)
