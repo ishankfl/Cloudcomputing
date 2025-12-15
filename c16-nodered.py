7dad83cc8f30ea4e324bc615d024894a46e02fb8
ishankfl <ishankafle5@gmail.com>
2025-12-15 11:04:11 +0545

    connected-to-mqtt-subscriber

--- c16-nodered.py
+++ c16-nodered.py
@@ -1,4 +1,5 @@
 import network
+from umqtt.simple import MQTTClient
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -8,3 +9,21 @@ while not wifi.isconnected():
     print('Connecting..')
 
 print("Successfully connected to wifi")
+
+MQTT_CLIENT_ID = 'ishan-client'
+MQTT_SERVER = 'broker.mqttdashboard.com'
+MQTT_TOPIC = 'ishan-topic-123'
+
+def msg_received(topic, msg):
+    print("Message comming from nodered: ", msg)
+
+mqtt_subscriber = MQTTClient(MQTT_CLIENT_ID,MQTT_SERVER)
+mqtt_subscriber.set_callback(msg_received)
+
+mqtt_subscriber.connect()
+
+mqtt_subscriber.subscribe(MQTT_TOPIC)
+
+while True:
+    mqtt_subscriber.check_msg()
+    print("Receiving.. data...")
