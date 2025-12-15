0cb26acfe2fbadc09eef08a10a302470f8dc129d
ishankfl <ishankafle5@gmail.com>
2025-12-15 15:00:50 +0545

    mqtt-connection-done

--- c14-workshop.py
+++ c14-workshop.py
@@ -1,5 +1,6 @@
 import network
 import time 
+from umqtt.simple import MQTTClient 
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -11,3 +12,22 @@ while True:
     print('.', end=' ')
     time.sleep(.3)
 print("Wifi connected successfully")
+
+mqtt_client_id = ''
+mqtt_broker = 'broker.mqttdashboard.com'
+mqtt_topic = ''
+
+def msg_receiver(topic, message):
+    print("Incomming Msg", message)
+
+mqtt_subscriber = MQTTClient(mqtt_client_id, mqtt_broker)
+mqtt_subscriber.set_callback(msg_receiver)
+
+mqtt_subscriber.connect()
+
+mqtt_subscriber.subscribe(mqtt_topic)
+
+while True:
+    mqtt_subscriber.check_msg()
+    print("Receiving..")
+    time.sleep(.5)
